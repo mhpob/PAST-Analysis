6246a7aaae30fb7aac76ed8cd33b11daaa47be6b
mhpob <mikeob9@gmail.com>
2015-01-26 15:35:58 -0500

    Separate out distance.csv creation.

---
+++ distances.R
@@ -0,0 +1,65 @@
+library(gdistance); library(raster); library(rgdal); library(dplyr)
+midstates <- shapefile(
+  'p:/obrien/gis/shapefiles/midatlantic/matl_states_land.shp')
+
+# Create nonsense raster file to clip shapefile
+ras.back <- raster(extent(-77.344, -69.8, 36.862, 42.872),
+                   resolution = 1/360, #5 arc-second grids = 720, 10 = 360
+                   vals = 1,
+                   crs = proj4string(midstates))
+mem.crop <- cbind(c(-75.8, -69.8, -69.8, -75.8), c(36.85, 36.85, 41.2, 36.85))
+mem.crop <- SpatialPolygons(list(Polygons(list(Polygon(mem.crop)),
+                                          'Memory-Wasting Ocean')),
+                            proj4string = CRS(proj4string(midstates)))
+
+ras.water <- mask(mask(ras.back, mem.crop, inverse = T),
+                  midstates, inverse = T)
+
+rm(midstates, ras.back, mem.crop)
+
+trans <- transition(ras.water, transitionFunction = function(x){1}, 16)
+geo <- geoCorrection(trans, type = 'c')
+
+load('secor.sb.rda')
+
+old <- secor.sb %>%
+  filter(tag.date < '2014-10-01', date.local >= '2014-04-15',
+         date.local <= '2014-09-15') %>%
+  group_by(station) %>%
+  summarize(lat = mean(lat), lon = mean(long)) %>%
+  as.data.frame()
+
+row.names(old) <- old[, 1]
+old <- old[, c(3, 2)]
+
+rm(trans, secor.sb)
+
+lc.dist <- function (trans, loc, res = c("dist", "path")){
+  # Code directly stolen then slightly edited from marmap package
+  if (res == "dist") {
+    cost <- costDistance(trans, as.matrix(loc))/1000
+    return(round(cost, digits = 2))
+  }
+    if (res == "path") {
+        nb.loc <- nrow(loc)
+        path <- list()
+        comb <- combn(1:nb.loc, 2)
+        pb <- txtProgressBar(min = 0, max = ncol(comb), style = 3)
+        for (i in 1:ncol(comb)) {
+            origin <- sp::SpatialPoints(loc[comb[1, i], ])
+            goal <- sp::SpatialPoints(loc[comb[2, i], ])
+            temp <- gdistance::shortestPath(trans, origin, goal, 
+                output = "SpatialLines")
+            path[[i]] <- temp@lines[[1]]@Lines[[1]]@coords
+            setTxtProgressBar(pb, i)
+        }
+        close(pb)
+        return(path)
+    }
+}
+
+distances <- lc.dist(geo, old, res = 'dist')
+paths <- lc.dist(geo, old, res = 'path')
+
+distances <- as.matrix(distances)
+write.csv(distances,'distances.csv')
