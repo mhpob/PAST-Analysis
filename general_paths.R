126ce6a2db32320f046aef98eaa2c134202ce6fb
mhpob <mikeob9@gmail.com>
2015-03-19 15:09:13 -0400

    Clean up path-creating function, adjust for new geographic arrays.

--- general_paths.R
+++ general_paths.R
@@ -1,86 +1,66 @@
 load('secor.sb.rda')
-
 library(dplyr)
-escapes <- secor.sb %>% 
-  filter(array %in% c('C&D', 'Delaware', 'Elk', 'Long Island', 'Mass',
+
+## Fish leaving Chesapeake Bay
+bay.escapes <- secor.sb %>% 
+  filter(array %in% c('C&D', 'DE Coast', 'Delaware', 'Long Island', 'Mass',
                       'New Jersey')) 
-escapes <- levels(factor(escapes$trans.num))
-escapes <- secor.sb %>% 
-  filter(trans.num %in% escapes) %>%
+bay.escapes <- levels(factor(bay.escapes$trans.num))
+bay.escapes <- secor.sb %>% 
+  filter(trans.num %in% bay.escapes) %>%
   arrange(date.local) %>% 
   data.frame
 
-escapes <- split(escapes, escapes$trans.num)
+bay.escapes <- split(bay.escapes, bay.escapes$trans.num)
 
-k <- function(data){
-  l <- data[1]
-  for(i in seq(1,length(data)-1)){
-    if(data[i] != data[i+1]){
-      l <- c(l, data[i+1])
-    }
-  }
-  l
-}
-
-k(escapes[['25490']][, 'array'])
-j <- escapes[['25490']]
-j[1,1]
-
-
-leavepot <- secor.sb %>% 
-  filter(!array %in% c('CBIBS', 'DDOE','Piney Point', 'Rt 301'))
-leavepot <- levels(factor(leavepot$trans.num))
-res <- secor.sb %>% 
-  filter(!trans.num %in% leave) %>%
-  arrange(date.local) %>% 
-  data.frame
-
-res <- split(res, res$trans.num)
-
-k <- function(data){
-  l <- data[1]
-  for(i in seq(1,length(data)-1)){
-    if(data[i] != data[i+1]){
-      l <- c(l, data[i+1])
-    }
-  }
-  l
-}
-
-k(res[[1]][, 'station'])
-length(res[[1]][,'station'])
-
-
-
-escapes <- secor.sb %>% 
-  filter(array %in% c('C&D', 'Delaware','Long Island', 'Mass',
-                      'New Jersey'))
-escapes <- levels(factor(escapes$trans.num))
-escapes <- secor.sb %>% 
-  filter(!trans.num %in% escapes,
-         array %in% c('CBL Pier', 'Cedar Point', 'Elizabeth', 'Elk', 'James',
-                      'Kent Island', 'Navy', 'Rappahannock', 'SERC', 'York'))
-escapes <- levels(factor(escapes$trans.num))
-escapes <- secor.sb %>%
-  filter(trans.num %in% escapes) %>% 
-  arrange(date.local) %>% 
-  data.frame
-
-escapes <- split(escapes, escapes$trans.num)
-
-k <- function(data){
+# Create a character vector with in the order of visted arrays
+gen.movement <- function(data){
   if(dim(data)[1] <= 1){
-    l <- data[1, 'array']
+    track <- data[1, 'array']
   }
   else{
-      l <- data[1, 'array']
-      for(i in seq(1,dim(data)[1]-1)){
-        if(data[i, 'array'] != data[i+1, 'array']){
-          l <- c(l, data[i+1, 'array'])
+      track <- data[1, 'array']
+      for(i in seq(1, dim(data)[1] - 1)){
+        if(data[i, 'array'] != data[i + 1, 'array']){
+          track <- c(track, data[i + 1, 'array'])
         }
       }
   }
-  l
+  track
 }
 
-lapply(escapes, k)
+lapply(bay.escapes, gen.movement)
+
+
+## Fish staying in the Potomac River
+pot.escapes <- secor.sb %>% 
+  filter(!array %in% c('Lower Potomac', 'Mid Potomac', 'Upper Potomac'))
+pot.escapes <- levels(factor(pot.escapes$trans.num))
+pot.residents <- secor.sb %>% 
+  filter(!trans.num %in% pot.escapes) %>%
+  arrange(date.local) %>% 
+  data.frame
+
+pot.residents <- split(pot.residents, pot.residents$trans.num)
+
+lapply(pot.residents, gen.movement)
+
+
+## Fish staying within the Chesapeake Bay
+escapes <- secor.sb %>% 
+  filter(array %in% c('C&D', 'DE Coast', 'Delaware', 'Long Island', 'Mass',
+                      'New Jersey'))
+escapes <- levels(factor(escapes$trans.num))
+bay.residents <- secor.sb %>% 
+  filter(!trans.num %in% escapes,
+         array %in% c('James', 'Lower MD Bay', 'Mid MD Bay', 'Other', 'Patuxent',
+                      'Rappahannock', 'Upper MD Bay', 'York'))
+bay.residents <- levels(factor(bay.residents$trans.num))
+bay.residents <- secor.sb %>%
+  filter(trans.num %in% bay.residents) %>% 
+  arrange(date.local) %>% 
+  data.frame
+
+bay.residents <- split(bay.residents, bay.residents$trans.num)
+
+lapply(bay.residents, gen.movement)
