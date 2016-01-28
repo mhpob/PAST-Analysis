1234ddf152d0008ee1bf0c14b206c137d91eb99b
mhpob <mikeob9@gmail.com>
2016-01-28 10:33:45 -0500

    Use TelemetryR::track. Change levels(factor()) to unique().

--- general_paths.R
+++ general_paths.R
@@ -1,69 +1,59 @@
 load('secor.sb.rda')
-library(dplyr)
+library(TelemetryR); library(dplyr)
 
 ## Fish leaving Chesapeake Bay
-bay.escapes <- secor.sb %>% 
+bay.escapes <- secor.sb %>%
   filter(array %in% c('Bay Mouth', 'C&D', 'DE Coast', 'Delaware', 'Long Island',
-                      'Mass', 'MD Coast', 'New Jersey', 'VA Coast')) 
-bay.escapes <- levels(factor(bay.escapes$trans.num))
-bay.escapes <- secor.sb %>% 
+                      'Mass', 'MD Coast', 'New Jersey', 'VA Coast'))
+bay.escapes <- unique(bay.escapes$trans.num)
+bay.escapes <- secor.sb %>%
   filter(trans.num %in% bay.escapes) %>%
-  arrange(date.local) %>% 
+  arrange(date.local) %>%
   data.frame
 
 bay.escapes <- split(bay.escapes, bay.escapes$trans.num)
 
 # Create a character vector with in the order of visted arrays
-gen.movement <- function(data){
-  if(dim(data)[1] <= 1){
-    track <- data[1, 'array']
-  }
-  else{
-      track <- data[1, 'array']
-      for(i in seq(1, dim(data)[1] - 1)){
-        if(data[i, 'array'] != data[i + 1, 'array']){
-          track <- c(track, data[i + 1, 'array'])
-        }
-      }
-  }
-  track
-}
-
-j <- lapply(bay.escapes, gen.movement)
-paste('Number that left =', length(j))
-paste('Number that got to Mass. =', length(grep('Mass', j)))
+escape.tracks <- lapply(bay.escapes, track, dates = 'date.local',
+            ids = 'array')
+paste('Number that left =', length(escape.tracks))
+paste('Number that got to Mass. =', length(grep('Mass', escape.tracks)))
 paste('Number that got to Long Island =',
-      length(intersect(grep('Mass', j, invert = T), grep('Island', j))))
+      length(intersect(grep('Mass', escape.tracks, invert = T),
+                       grep('Island', escape.tracks))))
 paste('Number that got to Delaware =',
-      length(intersect(grep('Island', j, invert = T), grep('De', j))))
+      length(intersect(grep('Island', escape.tracks, invert = T),
+                       grep('De', escape.tracks))))
 
 ## Fish staying in the Potomac River
-pot.escapes <- secor.sb %>% 
+pot.escapes <- secor.sb %>%
   filter(!array %in% c('Lower Potomac', 'Mid Potomac', 'Upper Potomac'))
-pot.escapes <- levels(factor(pot.escapes$trans.num))
-pot.residents <- secor.sb %>% 
+pot.escapes <- unique(pot.escapes$trans.num)
+pot.residents <- secor.sb %>%
   filter(!trans.num %in% pot.escapes) %>%
-  arrange(date.local) %>% 
+  arrange(date.local) %>%
   data.frame
 
 pot.residents <- split(pot.residents, pot.residents$trans.num)
 
-lapply(pot.residents, gen.movement)
+lapply(pot.residents, track, dates = 'date.local',
+            ids = 'array')
 
 
 ## Fish staying within the Chesapeake Bay
-escapes <- secor.sb %>% 
+escapes <- secor.sb %>%
   filter(array %in% c('Bay Mouth', 'C&D', 'DE Coast', 'Delaware', 'Long Island',
                       'Mass', 'MD Coast', 'New Jersey', 'VA Coast'))
-escapes <- levels(factor(escapes$trans.num))
-bay.residents <- secor.sb %>% 
-  filter(!trans.num %in% escapes)
-bay.residents <- levels(factor(bay.residents$trans.num))
+escapes <- unique(escapes$trans.num)
 bay.residents <- secor.sb %>%
-  filter(trans.num %in% bay.residents) %>% 
-  arrange(date.local) %>% 
+  filter(!trans.num %in% escapes)
+bay.residents <- unique(bay.residents$trans.num)
+bay.residents <- secor.sb %>%
+  filter(trans.num %in% bay.residents) %>%
+  arrange(date.local) %>%
   data.frame
 
 bay.residents <- split(bay.residents, bay.residents$trans.num)
 
-lapply(bay.residents, gen.movement)
+lapply(bay.residents, track, dates = 'date.local',
+            ids = 'array')
