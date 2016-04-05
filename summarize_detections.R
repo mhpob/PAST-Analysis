0526ab1fee11d56a9176271718b4001fedf84c88
mhpob <mikeob9@gmail.com>
2016-04-05 12:25:52 -0400

    Tabulate and summarize detections.

---
+++ summarize_detections.R
@@ -0,0 +1,37 @@
+load('secor.sb.rda')
+library(dplyr)
+number <- secor.sb %>%
+  group_by(array) %>%
+  summarize(det = n()) %>%
+  arrange(-det)
+
+fish <- secor.sb %>%
+  group_by(array) %>%
+  summarize(fish = length(unique(trans.num))) %>% 
+  arrange(-fish)
+
+# seasons dec-feb, mar-may june-aug, sept-nov
+# sexes
+secor.sb$season <- ifelse(lubridate::month(secor.sb$date.local) %in% c(3, 4, 5),
+                          'Spring',
+                   ifelse(lubridate::month(secor.sb$date.local) %in% c(6, 7, 8),
+                          'Summer',
+                   ifelse(lubridate::month(secor.sb$date.local) %in% c(9, 10, 11),
+                          'Fall', 'Winter')))
+season <- secor.sb %>%
+  group_by(array, season) %>%
+  summarize(fish = length(unique(trans.num))) %>% 
+  arrange(-fish)
+
+secor.sb$length.bin <- ifelse(secor.sb$length < 550, '<55',
+                      ifelse(secor.sb$length >= 550 &
+                               secor.sb$length < 650, '55-65',
+                      ifelse(secor.sb$length >= 650 &
+                               secor.sb$length < 800, '65-80', '>80')))
+
+season_size <- secor.sb %>%
+  group_by(array, season, length.bin) %>%
+  summarize(fish = length(unique(trans.num))) %>% 
+  arrange(-fish)
+
+
