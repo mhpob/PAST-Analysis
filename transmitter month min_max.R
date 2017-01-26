512bccfa5c16907e7cdc517c5b3f96c2bd13c759
cjwiernicki <cjwiernicki@gmail.com>
2017-01-26 10:38:37 -0500

    post split, defining min and max months

---
+++ transmitter month min_max.R
@@ -0,0 +1,36 @@
+library(TelemetryR); library(lubridate); library(dplyr)
+
+detects <- vemsort('p:/obrien/biotelemetry/detections')
+
+secor.sb <- detects %>% 
+  filter(trans.num >= 25434 & trans.num <= 25533 |
+         trans.num >= 53850 & trans.num <= 53899) %>%
+  select(-one_of('trans.name', 'trans.serial', 'sensor.value',
+                 'sensor.unit')) %>% 
+  data.frame()
+
+# secor.sb <- detects[(detects$trans.num >= 25434 & detects$trans.num <= 25533) |
+#                       (detects$trans.num >= 53850 & detects$trans.num <= 53899),]
+# secor.sb <- data.frame(secor.sb)[, 4:7]
+
+# Months b/w 1st and last detections per fish (transmitter)
+# 1) 1st and last dates per trans
+# 2) Subtract dates
+
+# 1) 
+# Split into a list according to transmitter
+?split
+class(secor.sb$transmitter)
+[1] "character"
+
+secor.sb$factor_transmitter <- factor(secor.sb$transmitter)
+
+# find min/max
+class(secor.sb$date.local)
+?month
+?year
+lapply(secor.sb$date.utc, min)
+lapply(secor.sb$date.utc, max)
+
+# 2) substract lists?
+
