e43d6af601ad4a33f8f4a651953b8634291a55f5
mhpob <mikeob9@gmail.com>
2017-01-26 13:25:55 -0500

    Redefine coastal arrays.

--- coastal_occur.R
+++ coastal_occur.R
@@ -3,17 +3,19 @@ load('secor.sb.rda')
 
 ## Coastal occurrance vs Length, including log fit
 occ.data <- secor.sb %>% 
-  filter(date.local >= '2014-03-30',
-         date.local <= '2014-10-29') %>% 
+  # filter(date.local >= '2014-03-30',
+  #        date.local <= '2014-10-29') %>%
   mutate(date.floor = lubridate::floor_date(date.local, unit = 'day'),
-         coastal = ifelse(array %in% c('MD Coast', 'DE Coast', 'Long Island',
-                                       'Mass', 'New Jersey'), 'YES', 'NO')) %>% 
-  distinct(trans.num, date.floor, coastal, .keep_all = T) %>% 
-  group_by(trans.num) %>% 
+         coastal = ifelse(array %in% c('VA Coast', 'MD Coast', 'DE Coast',
+                                'Hudson', 'Long Island', 'Mass', 'New Jersey'),
+                          'YES', 'NO')) %>% 
+  distinct(transmitter, date.floor, coastal, .keep_all = T) %>% 
+  group_by(transmitter) %>% 
   summarize(length = mean(length),
             coast = sum(coastal == 'YES'),
             bay = sum(coastal == 'NO'),
             prop = coast/(bay+coast)) %>% 
+  filter(coast + bay > 5) %>% 
   arrange(length)
             
 ggplot(data = occ.data, aes(x = length, y = prop)) + geom_point(size = 3) +
@@ -24,17 +26,18 @@ ggplot(data = occ.data, aes(x = length, y = prop)) + geom_point(size = 3) +
 
 ## Presence
 pres <- secor.sb %>% 
-  filter(date.local >= '2014-03-30',
-         date.local <= '2014-10-29') %>% 
-  mutate(coastal = ifelse(array %in% c('MD Coast', 'DE Coast', 'Long Island',
-                                       'Mass', 'New Jersey'), 1, 0)) %>% 
+  # filter(date.local >= '2014-03-30',
+  #        date.local <= '2014-10-29') %>% 
+  mutate(coastal = ifelse(array %in% c('VA Coast', 'MD Coast', 'DE Coast',
+                                'Hudson', 'Long Island', 'Mass', 'New Jersey'),
+                          1, 0)) %>%
   data.frame()
 
-coast <- levels(factor(pres[pres$coastal == 1, 'trans.num']))
+coast <- levels(factor(pres[pres$coastal == 1, 'transmitter']))
 
 pres <- pres %>% 
-  mutate(coastal = ifelse(trans.num %in% coast, 1, 0)) %>% 
-  distinct(trans.num, coastal, .keep_all = T)
+  mutate(coastal = ifelse(transmitter %in% coast, 1, 0)) %>% 
+  distinct(transmitter, coastal, .keep_all = T)
   
 ggplot(data = pres, aes(x = length, y = coastal)) + geom_point(size = 3) +
   stat_smooth(method = 'glm', method.args = list(family = 'binomial')) +
