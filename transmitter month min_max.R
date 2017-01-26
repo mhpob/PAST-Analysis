5f6c262079b9ccb1390ed77db91feab8e55f88cc
cjwiernicki <cjwiernicki@gmail.com>
2017-01-26 15:18:05 -0500

    Play with transmitters.

--- transmitter month min_max.R
+++ transmitter month min_max.R
@@ -9,6 +9,26 @@ secor.sb <- detects %>%
                  'sensor.unit')) %>% 
   data.frame()
 
+
+detects <- vemsort('p:/obrien/biotelemetry/detections')
+
+seq1 <- seq(25434, 25533, 1)
+seq2 <- seq(53850, 53899, 1)
+
+secor.sb <- detects$transmitter %>%
+  paste("A69-1601", seq1:seq, sep="-")%>%
+  select(-one_of('trans.name', 'trans.serial','sensor.value', 'sensor_unit
+  
+
+
+data.frame()
+ 
+
+
+
+
+
+
 # secor.sb <- detects[(detects$trans.num >= 25434 & detects$trans.num <= 25533) |
 #                       (detects$trans.num >= 53850 & detects$trans.num <= 53899),]
 # secor.sb <- data.frame(secor.sb)[, 4:7]
@@ -47,8 +67,31 @@ secor.sb_date_min
 
 
 
-v.secor.sb_date_max <- c(secor.sb_date_max)
-v.secor.sb_date_min <- c(secor.sb.date_min)
+#v.secor.sb_date_max <- c(secor.sb_date_max)
+#v.secor.sb_date_min <- c(secor.sb_date_min)
 
-v.secor.sb_date_mid <- diff(v.secor.sb_date_max, v.secor.sb_date_min)
+#v.secor.sb_date_mid <- diff(v.secor.sb_date_max, v.secor.sb_date_min)
+
+
+# 1b) Group w/in data frame
+
+secor.sb <- group_by(secor.sb, transmitter)
+
+test <- summarize(secor.sb,
+                  low = min(date.local),
+                  high = max(date.local))
+
+
+test$diff <- interval(test$low, test$high)
+
+ftest<-test$diff/dmonth
+
+ftest
+
+# hist
+
+
+hist(ftest, seq(0,148,1),col="green",main="")
+
+
 
