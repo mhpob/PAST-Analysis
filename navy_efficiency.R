209e64d7c80b7f45e8a418db65992afd10743b77
mhpob <mikeob9@gmail.com>
2017-02-07 15:08:48 -0500

    Season-specific Navy array efficiency.

--- navy_efficiency.R
+++ navy_efficiency.R
@@ -9,25 +9,30 @@ eff.base <- secor.sb %>%
          coastal = ifelse(array %in% c('VA Coast', 'MD Coast', 'DE Coast',
                                        'Hudson', 'Long Island', 'Mass',
                                        'New Jersey'), T, F),
-         mouth = ifelse(array == 'Bay Mouth', T, F)) %>% 
-  group_by(transmitter, yr.adjust) %>% 
-  filter(T %in% coastal) %>% 
-  summarize(navy.detected = T %in% mouth)
+         mouth = ifelse(array == 'Bay Mouth', T, F),
+         month = month(date.local),
+         mouth.season = ifelse(month %in% seq(4, 6, 1), 'Spring',
+                               ifelse(month %in% c(11, 12, 1),'Fall',
+                                      'Other'))) 
 
 overall.eff <- eff.base %>% 
+  group_by(transmitter, yr.adjust) %>% 
+  filter(T %in% coastal) %>%
+  summarize(navy.detected = T %in% mouth) %>% 
   ungroup() %>% 
   summarize(overall.eff = sum(navy.detected == T)/n())
   
 yr.eff <- eff.base %>% 
+  group_by(transmitter, yr.adjust) %>% 
+  filter(T %in% coastal) %>%
+  summarize(navy.detected = T %in% mouth) %>% 
   group_by(yr.adjust) %>% 
   summarize(yr.eff = sum(navy.detected == T)/n())
 
-season.eff <- ##need to begin from the beginning.
+season.eff <- eff.base %>% group_by(transmitter, yr.adjust) %>% 
+  filter(T %in% coastal) %>%
+  group_by(transmitter, yr.adjust, mouth.season) %>% 
+  summarize(navy.detected = T %in% mouth) %>% 
+  group_by(yr.adjust, mouth.season) %>% 
+  summarize(season.eff = sum(navy.detected == T)/n())
 
-# mutate(month = month(date.local),
-#        mouth.time = ifelse(mouth == T &
-#                              month %in% seq(4, 6, 1),
-#                            'Spring',
-#                            ifelse(mouth == T &
-#                                     month %in% c(11, 12, 1),
-#                                   'Fall', 'Other')))
