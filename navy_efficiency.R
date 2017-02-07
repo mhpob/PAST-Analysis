b223e1e11962eab77e84f3bfe124cb22db30a033
mhpob <mikeob9@gmail.com>
2017-02-07 12:00:50 -0500

    Rename navy efficiency variables, begin year- and season-specific efficiencies.

--- navy_efficiency.R
+++ navy_efficiency.R
@@ -1,7 +1,7 @@
 library(lubridate); library(ggplot2); library(dplyr)
 load('secor.sb.rda')
 
-data <- secor.sb %>% 
+eff.base <- secor.sb %>% 
   mutate(yr.adjust = ifelse(date.local <= '2015-03-21', 2014,
                             ifelse(date.local > '2015-03-21' &
                                      date.local <= '2016-03-21', 2015,
@@ -12,6 +12,22 @@ data <- secor.sb %>%
          mouth = ifelse(array == 'Bay Mouth', T, F)) %>% 
   group_by(transmitter, yr.adjust) %>% 
   filter(T %in% coastal) %>% 
-  summarize(navy.detected = T %in% mouth) %>% 
+  summarize(navy.detected = T %in% mouth)
+
+overall.eff <- eff.base %>% 
+  ungroup() %>% 
+  summarize(overall.eff = sum(navy.detected == T)/n())
+  
+yr.eff <- eff.base %>% 
   group_by(yr.adjust) %>% 
-  summarize(navy.eff = sum(navy.detected == T)/n())
+  summarize(yr.eff = sum(navy.detected == T)/n())
+
+season.eff <- ##need to begin from the beginning.
+
+# mutate(month = month(date.local),
+#        mouth.time = ifelse(mouth == T &
+#                              month %in% seq(4, 6, 1),
+#                            'Spring',
+#                            ifelse(mouth == T &
+#                                     month %in% c(11, 12, 1),
+#                                   'Fall', 'Other')))
