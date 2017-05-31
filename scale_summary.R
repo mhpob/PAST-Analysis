05259a22bf10908ee10bce4e4fe96745819a56d7
mhpob <mikeob9@gmail.com>
2017-05-31 15:01:16 -0400

    Calculate year classes.

--- scale_summary.R
+++ scale_summary.R
@@ -1,7 +1,7 @@
-library(dplyr)
+library(ggplot2); library(dplyr)
 
 ages <- read.csv('p:/obrien/biotelemetry/past sb/ages.csv')
-k <- ages %>%
+age.summary <- ages %>%
   group_by(FishID) %>% 
   summarize(ages = mean(Age_scale)) %>% 
   mutate(year = ifelse(grepl('2014', FishID), 2014, 2016)) %>% 
@@ -10,3 +10,17 @@ k <- ages %>%
             mean = mean(ages),
             median = median(ages),
             max = max(ages))
+
+
+year.class <- ages %>%
+  group_by(FishID) %>% 
+  summarize(ages = mean(Age_scale)) %>% 
+  filter(grepl('2014', FishID)) %>% 
+  mutate(year.class = floor(2014 - ages))
+
+
+ggplot() + geom_histogram(data = year.class, aes(year.class),
+                          binwidth = 1) +
+  scale_x_continuous(breaks = seq(2000, 2012, 2)) +
+  labs(x = 'Year Class', y = 'Count') +
+  theme_bw()
