c7d3a39674e564eba0f3e98115e165706aa2dda4
mhpob <mikeob9@gmail.com>
2017-06-19 17:30:35 -0400

    Clean up data import.

--- chain_bridge_wt.R
+++ chain_bridge_wt.R
@@ -1,28 +1,16 @@
 library(waterData)
 
-CB_discharge <- importDVs("01646500", code = "00010", stat = "00003",
-                          sdate = "2014-01-01", edate = "2016-12-31")
+cb_temp <- importDVs("01646500", code = "00010", stat = "00003",
+                     sdate = "2014-01-01", edate = "2016-12-31")
+cb_temp <- aggregate(val ~ dates, cb_temp, FUN = mean)
+cb_temp$var <- 'wtemp'
+# plot(cb_temp$dates, cb_temp$val)
 
 
-library(readxl)
-chain_bridge <- read_excel("P:/Wiernicki/Chain Bridge Flow Data.xlsx")
+cb_disch <- importDVs("01646500", code = "00060", stat = "00003",
+                     sdate = "2014-01-01", edate = "2016-12-31")
+cb_disch$val <- cb_disch$val * 0.02831685
+cb_disch$var <- 'discharge'
+# plot(cb_disch$dates, cb_disch$val)
 
-class(chain_bridge$`Date / Time`)
-
-#lubridate::ymd_hms(chain_bridge$`Date / Time`)
-
-
-# chain_bridge$`Date / Time` <- as.numeric(chain_bridge$`Date / Time`)
-# chain_bridge$`Discharge, ft^/s` <- as.numeric(chain_bridge$`Discharge, ft^/s`)
-# chain_bridge$`Gage height, ft` <- as.numeric(chain_bridge$`Gage height, ft`)
-# chain_bridge$`Temperature, water, degC, 4.1 ft from river bed (middle)` <- as.numeric(chain_bridge$`Temperature, water, degC, 4.1 ft from river bed (middle)`)
-# chain_bridge$`Temperature, water, deg C, 1 ft from river bed (bottom)` <- as.numeric(chain_bridge$`Temperature, water, deg C, 1 ft from river bed (bottom)`)
-# chain_bridge$`Temperature, water, deg C, 7.1 ft from river bed (top)` <- as.numeric(chain_bridge$`Temperature, water, deg C, 7.1 ft from river bed (top)`)
-# chain_bridge$`Specific conductance, wat unf uS/cm @ 25 degC` <- as.numeric(chain_bridge$`Specific conductance, wat unf uS/cm @ 25 degC`)
-# chain_bridge$`Nitrate & nitrite water in situ, mg/L as` <- as.numeric(chain_bridge$`Nitrate & nitrite water in situ, mg/L as`)
-# chain_bridge$`Turbidity, IR LED light, det ang 90 deg, FNU, From a multiparameter sonde` <- as.numeric(chain_bridge$`Turbidity, IR LED light, det ang 90 deg, FNU, From a multiparameter sonde`)
-# chain_bridge$`Dissolved oxygen, mg/L, From multiparameter sonde` <- as.numeric(chain_bridge$`Dissolved oxygen, mg/L, From multiparameter sonde`)
-# chain_bridge$`pH, water, unfltrd, field, std units, From multiparameter sonde` <- as.numeric(chain_bridge$`pH, water, unfltrd, field, std units, From multiparameter sonde`)
-# chain_bridge$`Temperature, water, degC, From multiparameter sonde` <- as.numeric(chain_bridge$`Temperature, water, degC, From multiparameter sonde`)
-# chain_bridge$`Specific conductance, wat unf uS/cm @ 25 deg C, From multiparameter sonde` <- as.numeric(chain_bridge$`Specific conductance, wat unf uS/cm @ 25 deg C, From multiparameter sonde`)
-
+chainbr <- merge(cb_temp, cb_disch, all = T)[, 1:3]
