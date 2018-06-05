b11fa208f6277ebb3119b914e902d576574e441a
mhpob <mikeob9@gmail.com>
2018-06-05 11:44:44 -0400

    Extend date to inlcude spring 2017.

--- tag_loss.R
+++ tag_loss.R
@@ -5,7 +5,7 @@ surv14s <- filter(secor.sb,
                   transmitter %in% paste0('A69-1601-', seq(25434, 25505, 1))) %>%
   trans_loss(., dates = 'date.local', group = 'transmitter',
              stdate = ymd_hms('2014-04-11 00:00:00'),
-             enddate = ymd_hms('2016-12-31 11:59:59')) %>% 
+             enddate = ymd_hms('2017-03-31 11:59:59')) %>% 
   mutate(tagging = 'Spring',
          pct = remaining / 72)
 
@@ -14,7 +14,7 @@ surv14f <- filter(secor.sb,
                   transmitter %in% paste0('A69-1601-', seq(25506, 25533, 1))) %>%
   trans_loss(., dates = 'date.local', group = 'transmitter',
              stdate = ymd_hms('2014-10-30 00:00:00'),
-             enddate = ymd_hms('2016-12-31 11:59:59')) %>% 
+             enddate = ymd_hms('2017-03-31 11:59:59')) %>% 
   mutate(tagging = 'Fall',
          pct = remaining / 28)
 
@@ -33,7 +33,19 @@ library(ggplot2)
 ggplot() + geom_line(data = surv14, aes(x = date, y = pct * 100,
                                         color = tagging)) +
   labs(x = 'Date', y = 'Percent 2014-tagged detected', color = 'Event') +
-  theme_bw()
+  scale_x_datetime(date_breaks = '6 month', date_labels = '%b %y') +
+  ylim(0, 100) +
+  theme_bw() +
+  theme(legend.position = c(0.8, 0.8))
+
+ggplot() + geom_line(data = surv14, aes(x = date, y = log(remaining),
+                                        color = tagging)) +
+  labs(x = 'Date', y = 'Natural log of fish remaining', color = 'Event') +
+  scale_x_datetime(date_breaks = '6 month', date_labels = '%b %y') +
+  theme_bw() +
+  theme(legend.position = c(0.8, 0.8))
+
+
 
 ggplot() + geom_line(data = surv16, aes(x = date, y = pct * 100)) +
   labs(x = 'Date', y = 'Percent 2016-tagged detected') +
