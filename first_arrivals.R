283c8d2394a66c37bd74d5b030dd87ce3e8a7ff8
mhpob <mikeob9@gmail.com>
2018-06-26 15:26:14 -0400

    Mixed effect modelling.

--- first_arrivals.R
+++ first_arrivals.R
@@ -59,16 +59,103 @@ pot.return <- base.data %>%
   filter(month(date.local) %in% 2:5,
          # date.local > '2014-06-01',
          array %in% c('Upper Potomac', 'Mid Potomac')) %>% 
-  mutate(year = year(date.local),
+  mutate(year = as.factor(year(date.local)),
          sex = ifelse(sex == '', 'Unknown', sex)) %>% 
-  group_by(transmitter, year) %>% 
+  group_by(transmitter, year, sex, length) %>% 
   summarize(wkfirst_pot = min(wk.num),
-            datefirst_pot = min(date.local))
+            datefirst_pot = min(date.local),
+            wklast_pot = max(wk.num),
+            datelast_pot = max(date.local),
+            duration = as.numeric(datelast_pot - datefirst_pot))
 
-ggplot() + geom_histogram(data = pot.return,
-                          aes(as.Date(yday(datefirst_pot), '2014-01-01')), bins = 13) +
+# bins = 14 if plotting first date only, 15 if first and last
+ggplot(data = pot.return) + geom_histogram(aes(as.Date(yday(datefirst_pot),
+                                                       '2014-01-01')), bins = 15,
+                                           fill = 'green', alpha = 0.7) +
+  geom_histogram(aes(as.Date(yday(datelast_pot),
+                             '2014-01-01')), bins = 15, fill = 'red', alpha = 0.7) +
   facet_wrap(~ year, ncol = 1) +
   scale_x_date(date_breaks = 'month', date_labels = '%b %d') +
   labs(x = 'Movement above Rt. 301', y = 'Count') +
   theme_bw()
-# ggsave('above301.png', width = 9, height = 7)
+
+# ggsave('301.png', width = 9, height = 7)
+
+
+
+# Spawning period ANOVAs----
+## Mean spawn date ~ year
+# Random transmitter effect isn't significant.
+m1 <- lme4::lmer(yday(datefirst_pot) ~ year + (1 | transmitter), data = pot.return)
+m2 <- lm(yday(datefirst_pot) ~ year, data = pot.return)
+anova(m1, m2)
+              
+# Use regular model.
+model <- aov(m2)
+summary(model)
+TukeyHSD(model)
+
+
+
+# Mean spawn date ~ sex
+library(lme4); library(emmeans)
+# Year should be random factor
+m1 <- lmer(yday(datefirst_pot) ~ (1 | year), data = pot.return)
+m2 <- lm(yday(datefirst_pot) ~ 1, data = pot.return)
+anova(m1, m2)
+
+# Model test
+model_sex <- lmer(yday(datefirst_pot) ~ sex + (1 | year), data = pot.return)
+model_null <- lmer(yday(datefirst_pot) ~ (1 | year), data = pot.return)
+anova(model_sex, model_null) #No difference
+
+# Just to show pairwise comparisons
+summary(model_sex)
+emmeans(model_sex, list(pairwise ~ sex), adjust = 'tukey')
+
+
+
+# Mean spawn date ~ size
+model_tl <- lmer(yday(datefirst_pot) ~ length + (1 | year), data = pot.return)
+model_null <- lmer(yday(datefirst_pot) ~ (1 | year), data = pot.return)
+
+anova(model_null, model_tl) # No difference
+
+
+
+# Duration ~ year
+# Random transmitter effect is significant when used with year as predictor
+m1 <- lmer(duration ~ year + (1 | transmitter), REML = F, data = pot.return)
+m2 <- lm(duration ~ year, data = pot.return)
+anova(m1, m2)
+
+
+model_dur <- lmer(duration ~ year + (1 | transmitter), data = pot.return)
+model_null <- lmer(duration ~ (1 | transmitter), data = pot.return)
+anova(model_dur, model_null)
+
+emmeans(model_dur, list(pairwise ~ year), adjust = 'tukey')
+
+
+
+# Duration ~ sex
+# Random year effect is significant
+model_sex <- lmer(duration ~ sex + (1 | year), data = pot.return)
+model_null <- lm(duration ~ sex, data = pot.return)
+anova(model_sex, model_null)
+
+model_null <- lmer(duration ~ (1 | year), data = pot.return)
+
+
+anova(model_sex, model_null)
+emmeans(model_sex, list(pairwise ~ sex), adjust = 'tukey')
+
+
+
+# Duration ~ size
+model_tl <- lmer(duration ~ length + (1 | year), data = pot.return)
+model_null <- lmer(duration ~ (1 | year), data = pot.return)
+
+anova(model_null, model_tl)
+
+
