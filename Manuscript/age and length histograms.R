ceed75ac5e472f49e10825d8fe69298dd58c3980
mhpob <mikeob9@gmail.com>
2019-12-11 11:13:53 -0500

    Change from mm to cm.

--- Manuscript/age and length histograms.R
+++ Manuscript/age and length histograms.R
@@ -6,7 +6,7 @@ ages <- read.csv('p:/obrien/biotelemetry/past sb/ages.csv',
                  stringsAsFactors = F) %>% 
   group_by(FishID, TL) %>% 
   summarize(age = mean(Age_scale)) %>% 
-  mutate(tlmm = TL * 25.4,
+  mutate(tlcm = TL * 2.54,
          collect = case_when(grepl('201410', FishID) ~ 'Fall',
                              grepl('2016', FishID) ~ '2016',
                              T ~ 'Spring'),
@@ -16,13 +16,13 @@ ages <- read.csv('p:/obrien/biotelemetry/past sb/ages.csv',
 # 2014 ----
 
 tl <- ggplot() + geom_histogram(data = filter(ages, grepl('2014', FishID)),
-                          aes(x = tlmm, fill = collect),
-                          breaks = seq(400, 1100, 50), color = 'black',
+                          aes(x = tlcm, fill = collect),
+                          breaks = seq(40, 110, 5), color = 'black',
                           position = 'stack', closed = 'right') +
   scale_fill_manual(values = c('white', 'darkgray')) +
-  geom_vline(xintercept = c(450, 600, 700, 800), linetype = 'dashed',
+  geom_vline(xintercept = c(45, 60, 70, 80), linetype = 'dashed',
              size = 1) +
-  labs(x = 'Total Length (mm)', y = 'Count') +
+  labs(x = 'Total Length (cm)', y = 'Count') +
   theme_bw() +
   theme(legend.position = 'none',
         axis.text = element_text(size = 15),
@@ -42,7 +42,7 @@ yrs <- ggplot() + geom_histogram(data = filter(ages, grepl('2014', FishID)),
         axis.title = element_text(size = 15))
 
 library(cowplot)
-plot_grid(tl, yrs)
+plot_grid(tl, yrs) # Exported at 775*375 resolution
 
 # 2016----
 # Not used in manuscript
