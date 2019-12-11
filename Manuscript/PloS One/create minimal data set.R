254148610344415bac80ffad33db4c5936939918
mhpob <mikeob9@gmail.com>
2019-12-11 17:05:16 -0500

    Add separate fish metadata file.

--- Manuscript/PloS One/create minimal data set.R
+++ Manuscript/PloS One/create minimal data set.R
@@ -3,8 +3,20 @@ load('secor.sb.rda')
 secor.sb <- secor.sb %>% 
   filter(date.local <= '2018-12-31',
          grepl('-25', transmitter)) %>% 
-  mutate(date.floor = lubridate::floor_date(date.local, 'day')) %>% 
-  distinct(transmitter, date.floor, station, .keep_all = T) %>% 
-  select(transmitter, date.floor, lat, long, array)
+  mutate(date.floor = lubridate::floor_date(date.local, 'day')) 
 
-write.csv(secor.sb, 'manuscript/plos one/Diff_Mig_CB_SB.csv', row.names = F)
+detections <- secor.sb %>% 
+  distinct(transmitter, date.floor, station, .keep_all = T) %>% 
+  select(transmitter, date.floor, lat, long, array) %>% 
+  rename(date = 'date.floor')
+
+write.csv(detections, 'manuscript/plos one/secor_detections.csv', row.names = F)
+
+
+
+tagging_data <- secor.sb %>% 
+  select(transmitter, tag.date:age) %>% 
+  distinct(transmitter, tag.date, .keep_all = T) %>% 
+  rename(length.mm = 'length', weight.kg = 'weight', age.yrs = 'age')
+
+write.csv(metadata, 'manuscript/plos one/secor_tagging_data.csv', row.names = F)
