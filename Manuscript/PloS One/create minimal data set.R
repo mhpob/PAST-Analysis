af9bcf332ef8953d7409fa432aa3d3f4feb9a0c1
mhpob <mikeob9@gmail.com>
2019-12-12 10:30:04 -0500

    Adjust for the double-tag.

--- Manuscript/PloS One/create minimal data set.R
+++ Manuscript/PloS One/create minimal data set.R
@@ -1,11 +1,18 @@
+library(dplyr)
 load('secor.sb.rda')
 
 secor.sb <- secor.sb %>% 
   filter(date.local <= '2018-12-31',
          grepl('-25', transmitter)) %>% 
-  mutate(date.floor = lubridate::floor_date(date.local, 'day')) 
+  mutate(date.floor = lubridate::floor_date(date.local, 'day'),
+         transmitter = case_when(transmitter == 'A69-1601-25465' &
+                                   date.utc <= '2014-06-15' ~ 'A69-1601-25465a',
+                                 transmitter == 'A69-1601-25465' &
+                                   date.utc > '2014-06-15' ~ 'A69-1601-25465b',
+                                 T ~ transmitter)) 
+  
 
-detections <- secor.sb %>% 
+ detections <- secor.sb %>% 
   distinct(transmitter, date.floor, station, .keep_all = T) %>% 
   select(transmitter, date.floor, lat, long, array) %>% 
   rename(date = 'date.floor')
@@ -19,4 +26,4 @@ tagging_data <- secor.sb %>%
   distinct(transmitter, tag.date, .keep_all = T) %>% 
   rename(length.mm = 'length', weight.kg = 'weight', age.yrs = 'age')
 
-write.csv(metadata, 'manuscript/plos one/secor_tagging_data.csv', row.names = F)
+write.csv(tagging_data, 'manuscript/plos one/secor_tagging_data.csv', row.names = F)
